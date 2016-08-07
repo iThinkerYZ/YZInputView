ce546ab47f02f6990b4288613d9613b76d59c330
iThinkerYZ <690423479@qq.com>
2016-08-07 18:50:48 +0800

    修复文本框遮住问题

--- YZInputViewDemo/YZInputViewDemo/ViewController.m
+++ YZInputViewDemo/YZInputViewDemo/ViewController.m
@@ -30,12 +30,14 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillChangeFrameNotification object:nil];
     
     // 设置文本框占位文字
-    _inputView.placeholder = @"请输入文字";
+    _inputView.placeholder = @"小码哥 iOS培训 吖了个峥";
+    _inputView.placeholderColor = [UIColor redColor];
     
     // 监听文本框文字高度改变
     _inputView.yz_textHeightChangeBlock = ^(NSString *text,CGFloat textHeight){
-        // 文本框文字高度改变会自动执行这个block，修改底部View的高度
-        // 设置底部条的高度 = 文字高度 + textView距离上下间距高度（10 = 上（5）下（5）间距总和）
+        // 文本框文字高度改变会自动执行这个【block】，可以在这【修改底部View的高度】
+        // 设置底部条的高度 = 文字高度 + textView距离上下间距约束
+        // 为什么添加10 ？（10 = 底部View距离上（5）底部View距离下（5）间距总和）
         _bottomHCons.constant = textHeight + 10;
     };
     
@@ -51,9 +53,10 @@
     
     // 获取键盘弹出时长
     CGFloat duration = [note.userInfo[UIKeyboardAnimationDurationUserInfoKey] floatValue];
+    CGFloat screenH = [UIScreen mainScreen].bounds.size.height;
     
     // 修改底部视图距离底部的间距
-    _bottomCons.constant = _bottomCons.constant == 0?endFrame.size.height:0;
+    _bottomCons.constant = endFrame.origin.y != screenH?endFrame.size.height:0;
     
     // 约束动画
     [UIView animateWithDuration:duration animations:^{
